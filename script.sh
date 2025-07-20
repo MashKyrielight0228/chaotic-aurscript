c260c4b32ae0cb740ddebb659a409a59f4b3ed85
krdt_nsh <113843763+MashKyrielight0228@users.noreply.github.com>
2025-07-20 15:17:52 +0800

    Create chaotic aur script.sh

---
+++ script.sh
@@ -0,0 +1,20 @@
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+sleep 1
+
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+sleep 1
+
+sudo su -
+
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+pacman -Syu
+
+sleep 1
+
+exit
